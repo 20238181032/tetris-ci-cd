1f864ff9323e0e92ee9fbda278ff6f172b4237f4
20238181032 <1352247240@qq.com>
2025-10-22 12:47:24 +0800

    Update App.vue

--- tetris-ci-cd/src/App.vue
+++ tetris-ci-cd/src/App.vue
@@ -1,1 +1,12 @@
+<template>
+  <div class="app">
+    <TetrisGame />
+  </div>
+</template>
+<script setup>
+import TetrisGame from './components/TetrisGame.vue'
+</script>
+<style>
+.app { text-align: center; margin-top: 20px; }
+</style>
 
